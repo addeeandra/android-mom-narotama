aade9895fd2b0b9b49917f797dea4837355dfefc
Aditya Chandra <inibukanadit@gmail.com>
2019-08-02 10:52:39 +0700

    adds name and group auto completion based on NIM at Other Submission Page

--- app/src/main/java/id/ac/narotama/mo/OtherActivity.kt
+++ app/src/main/java/id/ac/narotama/mo/OtherActivity.kt
@@ -3,6 +3,9 @@ package id.ac.narotama.mo
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import id.ac.narotama.mo.data.remote.APIEndpoint
 import kotlinx.android.synthetic.main.activity_other.*
 import kotlinx.coroutines.launch
 
@@ -18,11 +21,33 @@ class OtherActivity : AppActivity() {
 
     }
 
+    private val mNimTextWatcher by lazy {
+        object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                val nim = p0.toString()
+
+                // 04316034 - if length equal valid nim / 6
+                if (nim.length == 8) {
+                    fetchStudentByNim(nim)
+                }
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         btn_other_send.setOnClickListener {
             submitViolation()
         }
+        et_other_nim.addTextChangedListener(mNimTextWatcher)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        et_other_nim.removeTextChangedListener(mNimTextWatcher)
     }
 
     private fun showLoading() {
@@ -35,6 +60,23 @@ class OtherActivity : AppActivity() {
         et_other_nim.isEnabled = true
         et_other_violation.isEnabled = true
         et_other_punishment.isEnabled = true
+    }
+
+    private fun fetchStudentByNim(nim: String) {
+        launch {
+            showLoading()
+
+            val results = APIEndpoint.Fallback(this@OtherActivity) {
+                networkApi.getStudentList(nim)
+            }
+
+            hideLoading()
+
+            results?.firstOrNull()?.let { data ->
+                tv_other_name.text = data.nama
+                tv_other_team.text = data.kelompok
+            } ?: showMessage("Mahasiswa dgn NIM '$nim' tidak ditemukan")
+        }
     }
 
     private fun submitViolation() {
